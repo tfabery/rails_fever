ce22913d8a4816f047d93a3ad77f81bfc06d3704
Tony Fabery <tfabery@gmail.com>
2016-09-30 14:18:18 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     resources :comments, except: [:show, :index]
   end
 
+  resources :streamers
+
   get '/log-in' => 'sessions#new'
   post '/log-in' => 'sessions#create'
   get '/log-out' => 'sessions#destroy', as: :log_out
